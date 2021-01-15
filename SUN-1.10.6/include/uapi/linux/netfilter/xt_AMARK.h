8f584e80e59d8af4a819d76434181e0586e42962
Abdon Morales <39339393+Server2356@users.noreply.github.com>
2021-01-15 11:12:03 -0600

    Renames file with the new version system

---
+++ SUN-1.10.6/include/uapi/linux/netfilter/xt_AMARK.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _XT_MARK_H_target
+#define _XT_MARK_H_target
+
+#include <linux/netfilter/xt_mark.h>
+
+#endif /*_XT_MARK_H_target */
+
+/* Renamed since GitHub marks it as a case sensite conflict /*
+/* (C) 2021 Abdon Morales Jr /*
