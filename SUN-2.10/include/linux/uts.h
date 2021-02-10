cc6f0be05f928155abeba62d740180d76131ac26
Abdon Morales <39339393+Server2356@users.noreply.github.com>
2021-02-10 15:27:05 -0600

    [Final] New kernel RC update for v2.10 (Final RC Update) unless

--- SUN-2.10/include/linux/uts.h
+++ SUN-2.10/include/linux/uts.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+/* SPDX-License-Identifier: FRL-v1.0 */
 #ifndef _LINUX_UTS_H
 #define _LINUX_UTS_H
 
@@ -6,7 +6,7 @@
  * Defines for what uname() should return 
  */
 #ifndef UTS_SYSNAME
-#define UTS_SYSNAME "Sun" /* This is the name for the kernel */
+#define UTS_SYSNAME "Sun"
 #endif
 
 #ifndef UTS_NODENAME
